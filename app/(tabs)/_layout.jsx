c342045200c012b1b0d8366e80d1d1a03aae8a7d
Arkadiusz Adamowicz <arek.adamowicz81@gmail.com>
2024-05-22 01:34:37 +0200

    fix

--- app/(tabs)/_layout.jsx
+++ app/(tabs)/_layout.jsx
@@ -1,6 +1,6 @@
 import { View, Text, Image } from 'react-native';
 import React from 'react';
-import { Tabs, Redirect } from 'expo-router';
+import { Tabs } from 'expo-router';
 import { icons } from '../../constants';
 
 const TabIcon = ({ icon, color, name, focused }) => {
@@ -48,21 +48,6 @@ const TabsLayout = () => {
                 icon={icons.home}
                 color={color}
                 name='Home'
-                focused={focused}
-              />
-            ),
-          }}
-        />
-        <Tabs.Screen
-          name='bookmark'
-          options={{
-            title: 'Bookmark',
-            headerShown: false,
-            tabBarIcon: ({ color, focused }) => (
-              <TabIcon
-                icon={icons.bookmark}
-                color={color}
-                name='Bookmark'
                 focused={focused}
               />
             ),
